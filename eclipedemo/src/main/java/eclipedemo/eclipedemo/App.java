bcdc593b07bb4c19804513da9180a03d3198853e
satwa2306 <kongarasatwalekar@gmail.com>
2022-03-22 16:06:26 +0530

    first commit

---
+++ eclipedemo/src/main/java/eclipedemo/eclipedemo/App.java
@@ -0,0 +1,13 @@
+package eclipedemo.eclipedemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
